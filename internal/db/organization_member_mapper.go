f24f5396baa9e8aba5f91dc0303f58e883d3e269
pxguan <guanpx135@gmail.com>
2026-09-11 09:50:02 +0800

    refactor(db): 组织成员守卫 mapper 更名为 organization_member_mapper

--- internal/db/organization_member_mapper.go
+++ internal/db/organization_member_mapper.go
@@ -2,9 +2,9 @@ package db
 
 import "context"
 
-//go:generate go tool sqlmapgen -dir $PWD -mapper OrganizationMemberGuardMapper -sql ./organization_member_guard_mapper.xml -out ./organization_member_guard.sqlmap.gen.go -dialect postgres
+//go:generate go tool sqlmapgen -dir $PWD -mapper OrganizationMemberMapper -sql ./organization_member_mapper.xml -out ./organization_member_mapper.sqlmap.gen.go -dialect postgres
 
-type OrganizationMemberGuardMapper interface {
+type OrganizationMemberMapper interface {
 	LockOrganization(ctx context.Context, organizationUUID string) (string, error)
 	FindRole(ctx context.Context, organizationUUID, userReference string) (string, error)
 	FindMemberByReference(ctx context.Context, organizationUUID, userReference string) (AdminUser, error)
